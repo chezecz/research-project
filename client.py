6ffcd85722bda7ecd1b482fd41085467f33d6c66
chezecz <cheze92@gmail.com>
2018-08-12 21:13:47 +1000

    increase readability in client.py

--- client.py
+++ client.py
@@ -14,7 +14,21 @@ with io.open(file_name, 'rb') as audio:
     audio_content = audio.read()
     content = base64.b64encode(audio_content)
 
-jsonQuery = {'config': {'encoding':'LINEAR16','sampleRateHertz':'16000','languageCode':'en-US'}}, {'audio':{'content':content.decode("utf-8")}}
+jsonQuery = ({
+                'config': 
+                    {
+                        'encoding':'LINEAR16',
+                        'sampleRateHertz':'16000',
+                        'languageCode':'en-US'
+                    }
+                }, 
+            {
+                'audio':
+                    {
+                    'content':content.decode("utf-8")
+                    }
+            })
+
 s = json.dumps(jsonQuery)
 
 res = requests.post('http://127.0.0.1:5000/request/', json=s)
