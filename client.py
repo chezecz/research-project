387d38140bb5789c169a001fb0630eaf927d19cb
chezecz <cheze92@gmail.com>
2018-08-12 15:33:33 +1000

    update files

--- client.py
+++ client.py
@@ -4,8 +4,20 @@
 import base64
 import sys
 import json
+import os
+import io
 import requests
 
-def encode_audio(audio):
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.mp3')
+
+with io.open(file_name, 'rb') as audio:
     audio_content = audio.read()
-    return base64.b64encode(audio_content)
+    print(base64.b64encode(audio_content))
+    # return base64.b64encode(audio_content)
+
+# def encode_audio(audio):
+#     audio_content = audio.read()
+#     return base64.b64encode(audio_content)
