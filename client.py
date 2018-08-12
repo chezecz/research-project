176d02b4aeff61e911a3ea6d456630cd3122899e
chezecz <cheze92@gmail.com>
2018-08-12 19:23:51 +1000

    update client and server code with API key

--- client.py
+++ client.py
@@ -8,13 +8,13 @@ import requests
 file_name = os.path.join(
     os.path.dirname(__file__),
     'resources',
-    'audio.mp3')
+    'audio.wav')
 
 with io.open(file_name, 'rb') as audio:
     audio_content = audio.read()
     content = base64.b64encode(audio_content)
 
-jsonQuery = {'config': {'encoding':'mp3','sampleRateHertz':'16000','languageCode':'en-US'}}, {'audio':{'content':content.decode("utf-8")}}
+jsonQuery = {'config': {'encoding':'LINEAR16','sampleRateHertz':'16000','languageCode':'en-US'}}, {'audio':{'content':content.decode("utf-8")}}
 s = json.dumps(jsonQuery)
 
 res = requests.post('http://127.0.0.1:5000/request/', json=s)
--- server.py
+++ server.py
@@ -5,6 +5,16 @@ from flask import request
 
 from flask import Flask
 
+from apikey import apiKey
+
+baseURL = "https://speech.googleapis.com"
+requestURL = f"{baseURL}/v1/speech:recognize?key={apiKey}"
+
+def getTranscription(data):
+	req = requests.post(requestURL, json = data).json()
+	print(req)
+	return jsonify(req)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,10 +23,11 @@ def hello():
 
 @app.route("/request/", methods = ['POST'])
 def getRequest():
-	jsondata = request.get_json()
-	data = json.loads(jsondata)
-	print(data)
-	return jsonify(data)
+	# jsondata = request.get_json()
+	# data = json.loads(jsondata)
+	print(request.get_json())
+	print(getTranscription(request.get_json()))
+	return "Test"
 
 if __name__ == '__main__':
     app.env = "development"
