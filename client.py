17e069d0ee5d77e3d209bca348f33089ed75f8a7
chezecz <cheze92@gmail.com>
2018-08-12 15:18:08 +1000

    update server and client code

--- client.py
+++ client.py
@@ -2,38 +2,10 @@
 # export GOOGLE_APPLICATION_CREDENTIALS="/Users/cheze/python-XXXXXXXXXX.json"
 
 import base64
-import io
-import os
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-
-# Instantiates a client
-client = speech.SpeechClient()
-
-# The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
-
-# Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
-
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
-
-# Detects speech in the audio file
-response = client.recognize(config, audio)
-
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+import sys
+import json
+import requests
 
 def encode_audio(audio):
-	audio_content = audio.read()
-	return base64.b64encode(audio_content)
-
+    audio_content = audio.read()
+    return base64.b64encode(audio_content)
