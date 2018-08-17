12a01bf2610b30d59e2c6ad1141654316532c728
chezecz <cheze92@gmail.com>
2018-08-17 17:13:40 +1000

    create file for testing streaming google speech API

---
+++ stream_speech_api.py
@@ -0,0 +1,33 @@
+import io
+import sys
+import os
+import base64
+from google.cloud import speech
+
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    sys.argv[1])
+
+with io.open(file_name, 'rb') as audio:
+    audio_content = audio.read()
+    content = base64.b64encode(audio_content)
+
+client = speech.SpeechClient()
+config = speech.types.RecognitionConfig(
+	encoding='LINEAR16',
+	language_code='en-US',
+	sample_rate_hertz=44100,
+	)
+
+config = speech.types.StreamingRecognitionConfig(config=config)
+requests = [speech.types.StreamingRecognizeRequest(audio_content=content,)]
+
+responses = client.streaming_recognize(config, requests)
+print(responses)
+for response in responses:
+	for result in response:
+		for alternative in result.alternative:
+			print('=' * 20)
+			print('transcript: ' + alternative.transcript)
+			print('is final: ' + str(result.is_final))
