6323e7ee1ab3c2638723bc580c1be441348b302d
chezecz <cheze92@gmail.com>
2018-08-19 01:16:58 +1000

    delete unnecessary code

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ import io
 import requests
 import sys
 import time
-from contextlib import closing
 
 start_time = time.time()
 
@@ -16,12 +15,6 @@ file_name = os.path.join(
 
 with io.open(file_name, 'rb') as audio:
     audio_content = audio.read()
-
-# res = requests.post('http://127.0.0.1:5000/request/', data=audio_content, stream=True)
-# print(res.text)
-
-# with closing(requests.post('http://127.0.0.1:5000/request/', data=audio_content, stream=True)) as res:
-# 	print(res.next)
 
 for part in requests.post('http://127.0.0.1:5000/request/', data=audio_content, stream=True):
 	print (f"{part.decode('utf-8')}\n")
