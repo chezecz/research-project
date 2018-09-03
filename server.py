8ac8b937ac8302aad753a87fd6f99772678ddbe6
chezecz <cheze92@gmail.com>
2018-09-03 15:20:04 +1000

    fix issues with server code

--- server.py
+++ server.py
@@ -19,53 +19,38 @@ buffer = queue.Queue()
 buffer_response = queue.Queue()
 
 def chunks():
-    # while there is no silence:
-    # while we have regular buffer samples:
     while True:
         try:
-            yield buffer.get(timeout = 10)
+            yield buffer.get(timeout = 1)
         except queue.Empty:
-            print('break')
             break
 
 app = Flask(__name__)
 
 def get_transcription():
-    print("check")
-    generator = chunks()
-    client = speech.SpeechClient()
-    config = speech.types.RecognitionConfig(
-        encoding=Config.encoding,
-        language_code=Config.language,
-        sample_rate_hertz=Config.rate
-    )
-    config = speech.types.StreamingRecognitionConfig(config=config, interim_results = True)
-    requests = (speech.types.StreamingRecognizeRequest(audio_content=chunk) for chunk in generator)
-    results = client.streaming_recognize(config, requests)
+    while True:
+        generator = chunks()
+        client = speech.SpeechClient()
+        config = speech.types.RecognitionConfig(
+            encoding=Config.encoding,
+            language_code=Config.language,
+            sample_rate_hertz=Config.rate
+        )
+        config = speech.types.StreamingRecognitionConfig(config=config, interim_results = True)
+        requests = (speech.types.StreamingRecognizeRequest(audio_content=chunk) for chunk in generator)
+        results = client.streaming_recognize(config, requests)
 
-    for result in results:
-        for data in result.results:
-            for parts in data.alternatives:
-                print(parts)
-                buffer_response.put(parts.transcript)
-                # yield f"{delimeter}\n {parts.transcript}\n"
-
-
-def return_response():
-    generator = chunks_response()
-    for result in generator:
-        yield result
-
-def background_response():
-    thread = threading.Thread(target=return_response, args=())
-    thread.daemon = True
-    thread.start()
+        for result in results:
+            for data in result.results:
+                for parts in data.alternatives:
+                    buffer_response.put(parts.transcript)
 
 @app.before_first_request
 def activate_job():
     background = threading.Thread(target=get_transcription, args=())
     background.daemon = True
     background.start()
+    return background.is_alive()
 
 @app.route("/")
 def hello():
@@ -73,7 +58,7 @@ def hello():
 
 @app.route("/request/", methods = ['POST'])
 def get_request():
-    buffer.put(zlib.decompress(request.data))
+    buffer.put(zlib.decompress(request.data), block=False)
     if buffer_response.empty():
         return ""
     else:
