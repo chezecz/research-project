43e7793d1e2ffde26a8da72a76f12479a6782b41
chezecz <cheze92@gmail.com>
2018-09-08 22:45:22 +1000

    prototype for client-server using asyncio

--- asyncio_server.py
+++ asyncio_server.py
@@ -1,26 +1,64 @@
 import asyncio
+import zlib
 
-class ClientServerProtocol(asyncio.Protocol):
-    def __init__(self):
-        super.__init__()
+buffer = queue.Queue()
+buffer_response = queue.Queue()
+
+def chunks():
+    while True:
+        try:
+            yield buffer.get(timeout = 1)
+        except queue.Empty:
+            break
+
+def get_transcription():
+    while True:
+        generator = chunks()
+        client = speech.SpeechClient()
+        config = speech.types.RecognitionConfig(
+            encoding=Config.encoding,
+            language_code=Config.language,
+            sample_rate_hertz=Config.rate
+        )
+        config = speech.types.StreamingRecognitionConfig(config=config, interim_results = True)
+        requests = (speech.types.StreamingRecognizeRequest(audio_content=chunk) for chunk in generator)
+        results = client.streaming_recognize(config, requests)
+
+        for result in results:
+            for data in result.results:
+                for parts in data.alternatives:
+                    buffer_response.put(parts.transcript)
+
+def activate_job():
+    background = threading.Thread(target=get_transcription, args=())
+    background.daemon = True
+    background.start()
+
+class EchoServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        buffer.put(zlib.decompress(data), block=False)
+        if buffer_response.empty():
+            self.transport.sendto("", addr)
+        else:
+            self.transport.sendto(buffer_response.get(), addr)
 
 def run_server(host, port):
-        loop = asyncio.get_event_loop()
-        coro = loop.create_server(
-            ClientServerProtocol,
-            host, port
-        )
+    loop = asyncio.get_event_loop()
+    listen = loop.create_datagram_endpoint(
+        EchoServerProtocol, local_addr=('127.0.0.1', 9999))
+    transport, protocol = loop.run_until_complete(listen)
 
-        server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-
-        server.close()
-        loop.run_until_complete(server.wait_closed())
-        loop.close()
+    transport.close()
+    loop.close()
 
 if __name__ == '__main__':
+    activate_job()
     run_server('127.0.0.1', 8888)
