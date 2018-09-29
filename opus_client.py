7b5bafa4869db37c5b45e887be9b62d27b48d553
chezecz <cheze92@gmail.com>
2018-09-29 23:27:44 +1000

    support for opus codec

---
+++ opus_client.py
@@ -0,0 +1,44 @@
+import pyaudio
+import audioop
+import time
+import socket
+
+from opuslib import Encoder
+
+from config import Opus, Server
+
+enc = Encoder(Opus.rate, Opus.channels, 'audio')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def callback(input_data, frame_count, time_info, status):
+    audio_encoded = enc.encode(input_data, Opus.chunk)
+    get_transcription(audio_encoded)
+    return (input_data, pyaudio.paContinue)
+
+def record_audio():
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format = Opus.a_format,
+        channels = Opus.channels,
+        rate = Opus.rate,
+        input = True,
+        frames_per_buffer = Opus.chunk,
+        stream_callback = callback)
+
+    stream.start_stream()
+
+    while stream.is_active():
+        time.sleep(0.1)
+        data, addr = sock.recvfrom(1024)
+        if data:
+            print (data.decode())
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def get_transcription(data):
+    sock.sendto(data, (Server.host, Server.port))
+
+if __name__ == '__main__':
+    record_audio()
