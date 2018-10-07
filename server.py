8ede08eb1833a8ad2e4bdec7d6e5c990d35d48e2
chezecz <cheze92@gmail.com>
2018-10-07 23:16:00 +1100

    add new way to launch modules

---
+++ server.py
@@ -0,0 +1,4 @@
+from asyncio_arch.thread_asyncio_server import activate_job, run_server
+
+activate_job()
+run_server()
---
+++ stream.py
@@ -0,0 +1,3 @@
+from asyncio_arch.asyncio_stream import record_audio
+
+record_audio()
